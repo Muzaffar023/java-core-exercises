7d802b0a36e84925284b99cf29e265d37a6ca15a
Muzaffar023 <muzaffarxomitaliyev23@gmail.com>
2025-02-14 02:07:29 +0500

    Begin03 added

--- src/begins/Begin02.java
+++ src/begins/Begin02.java
@@ -6,10 +6,8 @@ public class Begin02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("a = ");
-        int butunSon = scanner.nextInt();
-
-        int natija = butunSon*4;
-
-        System.out.println("peremetri = 4*a = 4*" + butunSon + " = " + natija );
+        int son = scanner.nextInt();
+        int yuzasi = son*son;
+        System.out.printf("Yuzasi = a^2 = " + yuzasi );
     }
 }
--- src/begins/Begin03.java
+++ src/begins/Begin03.java
@@ -2,12 +2,16 @@ package begins;
 
 import java.util.Scanner;
 
-public class Begin03 {
+public class Begin02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("a = ");
-        int son = scanner.nextInt();
-        int yuzasi = son*son;
-        System.out.printf("Yuzasi = a^2 = " + yuzasi );
+        System.out.print(" a = ");
+        int a = scanner.nextInt();
+        System.out.print(" b = ");
+        int b = scanner.nextInt();
+        int p = (a+b)*2;
+        int s = a*b;
+        System.out.println(" p = " + p );
+        System.out.println(" S = " + s );
     }
 }
