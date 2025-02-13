9d5e50a63845d7f59df862f51e0afda925c63520
Muzaffar023 <muzaffarxomitaliyev23@gmail.com>
2025-02-14 02:08:43 +0500

    Begin04 added

---
+++ src/begins/Begin04.java
@@ -0,0 +1,14 @@
+package begins;
+
+import java.util.Scanner;
+
+public class Begin04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" d = ");
+        int d = scanner.nextInt();
+        double p = 3.14 ;
+        double L = p*d ;
+        System.out.print(" L = " + L );
+    }
+}
